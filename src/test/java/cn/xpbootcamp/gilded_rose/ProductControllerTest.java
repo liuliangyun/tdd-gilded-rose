861f5fd56887581f84c798340e7e7d42dc95c4e2
lyliu <lyliu@thoughtworks.com>
2020-03-22 15:52:03 +0800

    add test for decrease quality on 2.0 speed when sellIn less than or equal to 0

--- src/test/java/cn/xpbootcamp/gilded_rose/ProductControllerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductControllerTest.java
@@ -16,4 +16,15 @@ public class ProductControllerTest implements WithAssertions {
         assertThat(product).matches(p -> p.getSellIn() == 6 && p.getQuality() == 9);
     }
 
+    @Test
+    public void qualityShouldDecreaseOnDoubleSpeed() {
+        Product product = new Product("mock product");
+        product.setSellIn(0);
+        product.setQuality(10);
+
+        ProductController productController = new ProductController();
+        product = productController.decreaseSellInAndQuality(product);
+        assertThat(product).matches(p -> p.getSellIn() == -1 && p.getQuality() == 8);
+    }
+
 }
