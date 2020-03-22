106b2c40544736862b097fbc51ffdbac5d1bec4b
lyliu <lyliu@thoughtworks.com>
2020-03-22 17:40:53 +0800

    refactor method of changeSellInAndQuality

--- src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
@@ -5,39 +5,58 @@ import static java.util.Objects.isNull;
 public class ProductController {
 
     public Product changeSellInAndQuality(Product product) {
-        if (isNull(product) || product.getName().equals("Sulfuras")) {
-            return product;
+        if (isNull(product)) {
+            return null;
         }
 
         int rate = 1; //每天减少多少价值
-
         int sellIn = product.getSellIn();
         int quality = product.getQuality();
-        if (product.getName().equals("Aged Brie")) {
+
+        switch (product) {
+            case AGED_BRIE:
+                quality = quality + rate;
+                sellIn = sellIn - 1;
+                break;
+            case BACKSTAGE_PASS:
+                quality = changeBackstagePassQuality(sellIn, quality, rate);
+                sellIn = sellIn - 1;
+                break;
+            case NORMAL_PRODUCT:
+                quality = changeNormalProductQuality(sellIn, quality, rate);
+                sellIn = sellIn - 1;
+            default:
+                break;
+        }
+
+        product.setSellIn(sellIn);
+        product.setQuality(quality);
+        return product;
+    }
+
+    private int changeBackstagePassQuality(int sellIn, int quality, int rate) {
+        if (sellIn > 10) {
             quality = quality + rate;
         }
-        else if (product.getName().equals("Backstage pass")) {
-            if (sellIn > 10) {
-                quality = quality + rate;
-            }
-            else if (sellIn > 5) {
-                quality = quality + rate * 2;
-            }
-            else if (sellIn > 0) {
-                quality = quality + rate * 3;
-            }
-            else {
-                quality = 0;
-            }
+        else if (sellIn > 5) {
+            quality = quality + rate * 2;
         }
         else if (sellIn > 0) {
+            quality = quality + rate * 3;
+        }
+        else {
+            quality = 0;
+        }
+        return quality;
+    }
+
+    private int changeNormalProductQuality(int sellIn, int quality, int rate) {
+        if (sellIn > 0) {
             quality = quality - rate;
         }
         else {
             quality = quality - rate * 2;
         }
-        product.setSellIn(sellIn - 1);
-        product.setQuality(quality);
-        return product;
+        return quality;
     }
 }
