01dfefa87253acffa449efc91be5532d1b2e74b8
lyliu <lyliu@thoughtworks.com>
2020-03-22 15:53:31 +0800

    make all tests passed

--- src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
@@ -5,8 +5,14 @@ public class ProductController {
     public Product decreaseSellInAndQuality(Product product) {
         int sellIn = product.getSellIn();
         int quality = product.getQuality();
-        product.setSellIn(sellIn - 1);
-        product.setQuality(quality - 1);
+        if (sellIn > 0) {
+            product.setSellIn(sellIn - 1);
+            product.setQuality(quality - 1);
+
+        } else {
+            product.setSellIn(sellIn - 1);
+            product.setQuality(quality - 2);
+        }
         return product;
     }
 }
