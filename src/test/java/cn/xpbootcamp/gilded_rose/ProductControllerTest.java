abfe7244e90ae35e626fb576518a4ad6ed06ac70
lyliu <lyliu@thoughtworks.com>
2020-03-22 16:00:20 +0800

    refactor ProductControllerTest

--- src/test/java/cn/xpbootcamp/gilded_rose/ProductControllerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductControllerTest.java
@@ -1,9 +1,17 @@
 package cn.xpbootcamp.gilded_rose;
 
 import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ProductControllerTest implements WithAssertions {
+
+    private ProductController productController;
+
+    @BeforeEach
+    void setUp() {
+        productController = new ProductController();
+    }
 
     @Test
     public void qualityShouldDecrease() {
@@ -11,7 +19,6 @@ public class ProductControllerTest implements WithAssertions {
         product.setSellIn(7);
         product.setQuality(10);
 
-        ProductController productController = new ProductController();
         product = productController.decreaseSellInAndQuality(product);
         assertThat(product).matches(p -> p.getSellIn() == 6 && p.getQuality() == 9);
     }
@@ -22,7 +29,6 @@ public class ProductControllerTest implements WithAssertions {
         product.setSellIn(0);
         product.setQuality(10);
 
-        ProductController productController = new ProductController();
         product = productController.decreaseSellInAndQuality(product);
         assertThat(product).matches(p -> p.getSellIn() == -1 && p.getQuality() == 8);
     }
