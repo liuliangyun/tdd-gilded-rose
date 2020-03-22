cdb332ef3c2f89d3cb75994376e1e8f0d64a5fae
lyliu <lyliu@thoughtworks.com>
2020-03-22 15:38:54 +0800

    refactor ProductTest

--- src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
@@ -1,28 +1,33 @@
 package cn.xpbootcamp.gilded_rose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.assertj.core.api.WithAssertions;
 
 public class ProductTest implements WithAssertions {
 
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product("mock product");
+    }
+
     @Test
     public void qualityShouldSetWhenInRange0To50() {
-        Product product = new Product("mock product");
         product.setQuality(20);
         assertThat(product.getQuality()).isEqualTo(20);
     }
 
     @Test
     public void qualityShouldNotSetWhenLessThan0() {
-        Product product = new Product("mock product");
         product.setQuality(-1);
         assertThat(product.getQuality()).isEqualTo(0);
     }
 
     @Test
     public void qualityShouldNotSetWhenGreaterThan50() {
-        Product product = new Product("mock product");
         product.setQuality(51);
         assertThat(product.getQuality()).isEqualTo(0);
     }
