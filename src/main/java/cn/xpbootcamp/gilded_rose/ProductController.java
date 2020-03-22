503e48f2c16a9d7d190d320ee374758181582e71
lyliu <lyliu@thoughtworks.com>
2020-03-22 16:00:36 +0800

    refactor ProductController

--- src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/ProductController.java
@@ -3,16 +3,17 @@ package cn.xpbootcamp.gilded_rose;
 public class ProductController {
 
     public Product decreaseSellInAndQuality(Product product) {
+        int rate = 1; //每天减少多少价值
+
         int sellIn = product.getSellIn();
         int quality = product.getQuality();
         if (sellIn > 0) {
-            product.setSellIn(sellIn - 1);
-            product.setQuality(quality - 1);
-
+            quality = quality - rate;
         } else {
-            product.setSellIn(sellIn - 1);
-            product.setQuality(quality - 2);
+            quality = quality - rate * 2;
         }
+        product.setSellIn(sellIn - 1);
+        product.setQuality(quality);
         return product;
     }
 }
